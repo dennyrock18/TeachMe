1b33e98b27f434755ee950b2304896a716f22c5b
unknown <Denny>
2015-08-06 23:09:24 -0200

    Ticket and TicketVote nuevo modelo y migracion

---
+++ database/migrations/2015_08_07_005157_create_tikets_comments_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTiketsCommentsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('tikets_comments', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->mediumText('comment');
+			$table->string('link')->nullable();
+
+
+			$table->integer('user_id')->unsigned();
+			$table->foreign('user_id')->references('id')->on('users');
+
+			$table->integer('ticket_id')->unsigned();
+			$table->foreign('ticket_id')->references('id')->on('tickets');
+
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('tikets_comments');
+	}
+
+}
